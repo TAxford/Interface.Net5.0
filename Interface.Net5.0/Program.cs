31bb545734e555e7e68d918459b44869609a77fb
Timothy Axford <t.axford2@yahoo.com>
2023-04-03 11:08:24 -0400

    Created 2 ticket objects in the program class then called them to see if equal

--- Interface.Net5.0/Program.cs
+++ Interface.Net5.0/Program.cs
@@ -6,7 +6,9 @@ namespace Interface.Net5._0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Ticket t1 = new Ticket(10);
+            Ticket t2 = new Ticket(10);
+            Console.WriteLine(t2.Equals(t1));
         }
     }
 }
